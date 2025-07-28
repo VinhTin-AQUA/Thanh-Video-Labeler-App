7f79c7d5de4ac20632c3c1db9c440fe4ff8570c0
VinhTin-AQUA <tinhovinh@gmail.com>
2025-07-28 22:30:19 +0700

    update

--- src/app/pages/labeling-details/labeling-details.ts
+++ src/app/pages/labeling-details/labeling-details.ts
@@ -1,4 +1,12 @@
-import { Component, Input } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    EventEmitter,
+    Input,
+    Output,
+    SimpleChanges,
+    ViewChild,
+} from '@angular/core';
 import { VideoInfo } from '../labeling/models/videoInfo';
 import { FormsModule } from '@angular/forms';
 import { VideoInforService } from '../labeling/services/video-infor-service';
@@ -13,7 +21,10 @@ import { environment } from '../../../environments/environment.development';
 })
 export class LabelingDetails {
     @Input() videoInfo!: VideoInfo;
-    videoUrl = environment.videoBaseUrl + '/VideoFolder'
+    videoUrl = environment.videoBaseUrl + '/VideoFolder';
+    @Output() nextVideo = new EventEmitter<void>();
+    @Output() prevVideo = new EventEmitter<void>();
+    @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
 
     constructor(private videoInforService: VideoInforService) {}
 
@@ -28,5 +39,26 @@ export class LabelingDetails {
                     showGlobalDialog('Update', err.error.message, false);
                 },
             });
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (!changes['videoInfo'].previousValue) {
+            return;
+        }
+        const currentTransID = changes['videoInfo'].currentValue.transID;
+        const previousTransID = changes['videoInfo'].previousValue.transID;
+
+        if (currentTransID !== previousTransID && this.videoPlayer) {
+            // Gọi load() để reload video
+            this.videoPlayer.nativeElement.load();
+        }
+    }
+
+    onNextClick() {
+        this.nextVideo.emit();
+    }
+
+    onPrevClick() {
+        this.prevVideo.emit();
     }
 }
--- src/app/pages/labeling/labeling.ts
+++ src/app/pages/labeling/labeling.ts
@@ -1,11 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ConvertToQueryString } from '../../helper/convertToQueryString';
 import { VideoInforService } from './services/video-infor-service';
 import { VideoInfo } from './models/videoInfo';
 import { LabelingDetails } from '../labeling-details/labeling-details';
-import { hideGlobalLoading, showGlobalLoading } from '../../shared/signals/loading-signal';
-import { hideGlobalDialog } from '../../shared/signals/dialog-signal';
+import {
+    hideGlobalLoading,
+    showGlobalLoading,
+} from '../../shared/signals/loading-signal';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-labeling',
@@ -26,19 +29,40 @@ export class Labeling {
     clearAllData = false;
 
     videos = signal<VideoInfo[]>([]);
+    selectedVideo: VideoInfo | null = null;
+    selectedIndex: number = 0;
 
-    constructor(private videoInforService: VideoInforService) {}
+    constructor(
+        private videoInforService: VideoInforService,
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private cdr: ChangeDetectorRef
+    ) {}
 
     ngOnInit() {
-        this.filter();
+        this.activatedRoute.queryParams.subscribe((res: any) => {
+            if (res.page) {
+                this.filters.page = res.page;
+            }
+
+            if (res.pageSize) {
+                this.filters.pageSize = res.pageSize;
+            }
+            this.cdr.detectChanges();
+            this.filter(false);
+        });
     }
 
-    private filter() {
+    private filter(canShowVideo: boolean) {
         this.videoInforService
             .getVideo(ConvertToQueryString(this.filters))
             .subscribe({
                 next: (res: any) => {
                     this.videos.update((x) => res.data);
+
+                    if (canShowVideo) {
+                        this.selectedVideo = this.videos()[0];
+                    }
                 },
                 error: (err) => {
                     console.log(err.error);
@@ -47,25 +71,38 @@ export class Labeling {
     }
 
     applyFilter() {
-        this.filter();
+        this.filter(false);
     }
 
     nextPage() {
         this.filters.page++;
-        this.filter();
+        this.router.navigate([], {
+            relativeTo: this.activatedRoute,
+            queryParams: { page: this.filters.page },
+            queryParamsHandling: 'merge', // giữ các query params khác
+        });
     }
 
     prevPage() {
         if (this.filters.page > 1) {
             this.filters.page--;
-            this.filter();
+
+            this.router.navigate([], {
+                relativeTo: this.activatedRoute,
+                queryParams: { page: this.filters.page },
+                queryParamsHandling: 'merge', // giữ các query params khác
+            });
         }
     }
 
-    selectedVideo: VideoInfo | null = null;
-
     openVideoModal(video: VideoInfo) {
         this.selectedVideo = video;
+        const index = this.videos().findIndex(
+            (item) => item.transID === video.transID
+        );
+        this.selectedIndex = index;
+
+        console.log(this.selectedIndex);
 
         if (this.selectedVideo) {
             document.body.classList.add('overflow-hidden');
@@ -108,8 +145,27 @@ export class Labeling {
                 error(err) {
                     hideGlobalLoading();
                     console.error('Lỗi khi tải file:', err);
-                    
                 },
             });
+    }
+
+    onNext() {
+        if (this.selectedIndex < this.videos().length - 1) {
+            this.selectedIndex++;
+            this.selectedVideo = this.videos()[this.selectedIndex];
+            console.log(this.selectedVideo.transID);
+        } else {
+            this.selectedIndex = 0;
+            this.filters.page++;
+            this.filter(true);
+        }
+    }
+
+    onPrev() {
+        if (this.selectedIndex > 0) {
+            this.selectedIndex--;
+            this.selectedVideo = this.videos()[this.selectedIndex];
+            console.log(this.selectedVideo.transID);
+        }
     }
 }
