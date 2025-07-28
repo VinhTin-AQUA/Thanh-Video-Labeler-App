de6ace14fa173998d79ede3f9a531726164fc443
VinhTin-AQUA <tinhovinh@gmail.com>
2025-07-28 08:14:47 +0700

    update port

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    baseUrl: 'http://localhost:5195/api',
-    videoBaseUrl: 'http://localhost:5195',
-    videoDowloadHubUrl: 'http://localhost:5195/VideoDowloadHub',
+    baseUrl: 'http://localhost:5000/api',
+    videoBaseUrl: 'http://localhost:5000',
+    videoDowloadHubUrl: 'http://localhost:5000/VideoDowloadHub',
 };
